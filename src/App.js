1165aae508f81b50e9539ca8153070f4746ad622
Usman <usman@trustwise.ai>
2024-10-03 03:03:21 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,20 +2,25 @@ import { useEffect, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { DynamicForm } from "./DynamicForm";
 import "./App.css";
+import axios from "axios";
 
 const App = () => {
-  const [formData, setFormData] = useState({});
+  const [schemas, setSchemas] = useState({});
   const [loading, setLoading] = useState(true)
   useEffect(() => {
     fetchData();
   }, []);
 
+
+
+  const id = `66fdc0a2600909ecccb2296b`
+
   const fetchData = async () => {
     setLoading(true)
     try {
-      const response = await fetch("https://api.ameerpetit.com/api/entities/items/jsonform/66fcf4fd3a366a8c48d82d3b");
+      const response = await fetch(`https://api.ameerpetit.com/api/entities/items/schema/${id}`);
       const data = await response.json();
-      setFormData(data.data);
+      setSchemas(data);
     } catch (error) {
       console.error("Error fetching data:", error);
     } finally {
@@ -23,8 +28,32 @@ const App = () => {
     }
   };
 
+
+
+
+
   const handleSubmit = (formData) => {
-    console.log("Form submitted:", formData);
+    const reqData = {
+      type: schemas?.name,
+      name: "submitted_data",
+      data: formData,
+      "header": {
+        host: window.location.hostname,
+        "userId": "superadmin@ameerpetit.com"
+      },
+    }
+
+    axios.post(`https://api.ameerpetit.com/api/entities/items`, reqData,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: '*',
+        },
+      }
+    ).then((response) => {
+      console.log("Response:", response.data);
+      alert("Form submitted successfully!")
+    }).catch((error) => { console.error("Error submitting form:", error); })
   };
 
 
@@ -33,8 +62,8 @@ const App = () => {
     <Container fluid="lg" className="shadow-lg py-4 px-5 my-5">
       <Row>
         <Col md={12}>
-          <h1 className="text-center text-theme">JSON FORM</h1>
-          {loading ? "Loading Data Please wait..." : <DynamicForm formData={formData} onSubmit={handleSubmit} />}
+          <h1 className="text-center text-theme"> {schemas?.name ?? "JSON FORM"}</h1>
+          {loading ? "Loading Data Please wait..." : <DynamicForm schemas={schemas} onSubmit={handleSubmit} />}
         </Col>
       </Row>
     </Container>
@@ -42,3 +71,4 @@ const App = () => {
 };
 
 export default App;
+
--- src/DynamicForm.js
+++ src/DynamicForm.js
@@ -1,12 +1,16 @@
-import React, { useState, useEffect } from "react";
-import { Container, Row, Col, Form, Button } from "react-bootstrap";
-import { JsonForms } from "@jsonforms/react";
 import {
-  materialRenderers,
   materialCells,
+  materialRenderers,
 } from "@jsonforms/material-renderers";
+import { JsonForms } from "@jsonforms/react";
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
-export const DynamicForm = ({ formData, onSubmit }) => {
+export const DynamicForm = ({ schemas, onSubmit }) => {
+
+
+  const [formData, setFormData] = useState(null)
+
   const handleSubmit = (e) => {
     e.preventDefault();
     onSubmit(formData);
@@ -15,13 +19,14 @@ export const DynamicForm = ({ formData, onSubmit }) => {
   return (
     <Form onSubmit={handleSubmit}>
       <JsonForms
-        data={formData.data}
-        schema={formData.schema}
-        uischema={formData.uischema}
+        data={schemas?.data && Object.keys(schemas?.data).length > 0 ? schemas?.data : undefined}
+        schema={schemas?.schema && Object.keys(schemas?.schema).length > 0 ? schemas?.schema : undefined}
+        uischema={schemas?.uischema && Object.keys(schemas?.uischema).length > 0 ? schemas?.uischema : undefined}
         renderers={materialRenderers}
         cells={materialCells}
-        onChange={({ data }) => onSubmit(data)}
+        onChange={({ data }) => { if (data) setFormData(data) }}
       />
+
       <Button className="bth-theme ms-auto d-block w-auto" type="submit">
         Submit
       </Button>
