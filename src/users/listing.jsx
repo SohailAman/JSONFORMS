046e56e0659e9612b6f9334255184260d2267e48
Sohail Aman <sohailaman121@gmail.com>
2024-10-05 04:34:02 +0500

    added loader

--- src/users/listing.jsx
+++ src/users/listing.jsx
@@ -36,47 +36,50 @@ const Users = () => {
 
                 <Link to={`/user/add`} className='bth-theme btn'>Add User</Link>
             </div>
-            <Row className="d-flex justify-content-center align-items-center">
-                {userData?.length > 0 ? userData?.map((data) => (
 
-                    <Col md={6} xl={4}>
-                        <Card className="text-center" style={{ borderRadius: '15px' }}>
-                            <Card.Body>
-                                <div className="mt-3 mb-4">
-                                    <img
-                                        src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava2-bg.webp"
-                                        className="rounded-circle img-fluid"
-                                        style={{ width: '100px' }}
-                                        alt="Profile"
-                                    />
-                                </div>
-                                <h4 className="mb-2">{data?.data?.first_name} {data?.data?.last_name}</h4>
-                                <p className="text-muted mb-4">
-                                    @Programmer ~{data?.data?.age}
-                                </p>
+            {loading ? <p>Loading...</p> :
+                <Row className="d-flex justify-content-center align-items-center">
+                    {userData?.length > 0 ? userData?.map((data) => (
 
-                                <div className="mb-4 pb-2">
-                                    <Button variant="outline-primary" className="btn-floating me-2">
-                                        <FaFacebookF className="fa-lg" />
+                        <Col md={6} xl={4}>
+                            <Card className="text-center" style={{ borderRadius: '15px' }}>
+                                <Card.Body>
+                                    <div className="mt-3 mb-4">
+                                        <img
+                                            src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava2-bg.webp"
+                                            className="rounded-circle img-fluid"
+                                            style={{ width: '100px' }}
+                                            alt="Profile"
+                                        />
+                                    </div>
+                                    <h4 className="mb-2">{data?.data?.first_name} {data?.data?.last_name}</h4>
+                                    <p className="text-muted mb-4">
+                                        @Programmer ~{data?.data?.age}
+                                    </p>
+
+                                    <div className="mb-4 pb-2">
+                                        <Button variant="outline-primary" className="btn-floating me-2">
+                                            <FaFacebookF className="fa-lg" />
+                                        </Button>
+                                        <Link to={data?.data?.email} target='_blank' className="btn btn-floating me-2">
+                                            <BiEnvelope className="fa-lg" />
+                                        </Link>
+                                        <Button variant="outline-primary" className="btn-floating">
+                                            <FaSkype className="fa-lg" />
+                                        </Button>
+                                    </div>
+
+                                    <Button variant="primary" className="btn-rounded btn bth-theme">
+                                        View Profile
                                     </Button>
-                                    <Link to={data?.data?.email} target='_blank' className="btn btn-floating me-2">
-                                        <BiEnvelope className="fa-lg" />
-                                    </Link>
-                                    <Button variant="outline-primary" className="btn-floating">
-                                        <FaSkype className="fa-lg" />
-                                    </Button>
-                                </div>
-
-                                <Button variant="primary" className="btn-rounded btn bth-theme">
-                                    View Profile
-                                </Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                )
-                )
-                    : <p>No Users Found</p>}
-            </Row>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    )
+                    )
+                        : <p>No Users Found</p>}
+                </Row>
+            }
         </Container>
 
     )
